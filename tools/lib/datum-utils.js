c6e934aa3849fb742314d3b4390d906b7d212ba0
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:06:16 -0500

    Update datum-utils.js

--- tools/lib/datum-utils.js
+++ tools/lib/datum-utils.js
@@ -1,1 +1,18 @@
+function round3(n) {
+  return Math.round(Number(n) * 1000) / 1000;
+}
 
+function buildThresholdsNAVD88(gauge) {
+  const m = gauge.thresholdsMLLW;
+  const navd = Number(gauge.offsetsFromMLLW.NAVD88 || 0);
+  return {
+    minorLow: round3(m.minorLow + navd),
+    moderateLow: round3(m.moderateLow + navd),
+    majorLow: round3(m.majorLow + navd)
+  };
+}
+
+module.exports = {
+  round3,
+  buildThresholdsNAVD88
+};
