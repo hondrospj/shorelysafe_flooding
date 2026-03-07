a9e4b6f7d6841b248a3399273ab37d6310f9d59e
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:06:37 -0500

    Update slug-utils.js

--- tools/lib/slug-utils.js
+++ tools/lib/slug-utils.js
@@ -1,1 +1,10 @@
+function slugify(s) {
+  return String(s)
+    .trim()
+    .toLowerCase()
+    .replace(/&/g, "and")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
 
+module.exports = { slugify };
