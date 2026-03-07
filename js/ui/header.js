5efa7d1d623148f9bb01b1a0ec2d04137383e2a1
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:30:11 -0500

    Update header.js

--- js/ui/header.js
+++ js/ui/header.js
@@ -1,1 +1,14 @@
-
+export function renderHeader(gauge) {
+  return `
+    <div class="topbar">
+      <div class="top-left">
+        <div class="kicker">ShorelySafe</div>
+        <h1>${gauge.shortName}, NJ</h1>
+        <p class="sub">${gauge.name}</p>
+      </div>
+      <div class="pill">
+        USGS ${gauge.usgsId} · NOAA ${gauge.noaaId} · ${gauge.noaaAcronym}
+      </div>
+    </div>
+  `;
+}
