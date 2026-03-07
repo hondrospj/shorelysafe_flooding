5d263cbf4d7ea536999f74573306cba2239b0041
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:26:22 -0500

    Update file-utils.js

--- tools/lib/file-utils.js
+++ tools/lib/file-utils.js
@@ -1,1 +1,20 @@
+const fs = require("fs");
 
+function ensureDir(dir) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+function readJSON(filePath, fallback = null) {
+  if (!fs.existsSync(filePath)) return fallback;
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+}
+
+function writeJSON(filePath, obj) {
+  fs.writeFileSync(filePath, JSON.stringify(obj, null, 2) + "\n", "utf8");
+}
+
+module.exports = {
+  ensureDir,
+  readJSON,
+  writeJSON
+};
