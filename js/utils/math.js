6698452ed21136791c5e298f80c6082efbebd1db
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:29:56 -0500

    Update math.js

--- js/utils/math.js
+++ js/utils/math.js
@@ -1,1 +1,8 @@
-
+export function maxBy(arr, fn) {
+  if (!arr?.length) return null;
+  let best = arr[0];
+  for (const item of arr) {
+    if (fn(item) > fn(best)) best = item;
+  }
+  return best;
+}
