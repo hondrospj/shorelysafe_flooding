e11782b7616404e1bba5f4a23868f5c193648425
hondrospj <hondrosp@go.stockton.edu>
2026-03-06 20:29:11 -0500

    Update datum.js

--- js/datum.js
+++ js/datum.js
@@ -1,1 +1,19 @@
+export function buildThresholdSets(gauge) {
+  const m = gauge.thresholdsMLLW;
+  const off = gauge.offsetsFromMLLW;
 
+  function add(offset) {
+    return {
+      minorLow: +(m.minorLow + offset).toFixed(2),
+      moderateLow: +(m.moderateLow + offset).toFixed(2),
+      majorLow: +(m.majorLow + offset).toFixed(2)
+    };
+  }
+
+  return {
+    MLLW: add(off.MLLW || 0),
+    MSL: add(off.MSL || 0),
+    MHHW: add(off.MHHW || 0),
+    NAVD88: add(off.NAVD88 || 0)
+  };
+}
